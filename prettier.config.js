d215eca73b38d3c26250e2915b0963eb0d8ecb72
José Manuel Lucas <josemanuel@jmlweb.es>
2025-01-28 20:43:10 +0100

    feat: initialize project

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
+
+module.exports = config;
